be721e2721ba4f20c412012c577040208e36b72f
Danazumi <granzin98@gmail.com>
2026-03-11 11:11:27 +0100

    commit 1

--- src/product/product.schema.ts
+++ src/product/product.schema.ts
@@ -1,23 +1,21 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Document } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-export type ProductDocument = Product & Document
+export type ProductDocument = Product & Document;
 
 //we'll annotate our class as a schema
 @Schema()
 export class Product {
-    @Prop({required: true})
-    name:string
-    @Prop({required: true})
-    price:number
-    @Prop()
-    description:string
-
-} 
+  @Prop({ required: true })
+  name: string;
+  @Prop({ required: true })
+  price: number;
+  @Prop()
+  description: string;
+}
 
 //Here we generate Mongoose Schema
-export const ProductSchema = SchemaFactory.createForClass(Product)
+export const ProductSchema = SchemaFactory.createForClass(Product);
 
 //ID is auto generated for us
 // @Prop tells mongoose how to store the property
-//model is an instance of that schema that interacts with the database
