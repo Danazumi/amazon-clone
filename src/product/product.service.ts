cfa3919f29a0abd961fb5fa2fc4f63219fdd4a1f
Danazumi <granzin98@gmail.com>
2026-03-13 13:16:59 +0100

    add softDelete

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -3,60 +3,60 @@ import { InjectModel } from '@nestjs/mongoose';
 import { ProductDocument } from './product.schema';
 import { Model, DeleteResult } from 'mongoose';
 
-
-
 @Injectable()
 export class ProductService {
-    constructor(@InjectModel('Product') private readonly productModel: Model<ProductDocument> ){}
+  constructor(
+    @InjectModel('Product')
+    private readonly productModel: Model<ProductDocument>,
+  ) {}
 
-    // create the productModel
-    async create(       
-        name: string, 
-        price: number, 
-        description?: string, 
-    ): Promise<ProductDocument>{
-        //instanciate new object by ref to d productModel
-        const newProduct = new this.productModel({name, price, description})
-        return newProduct.save()
+  // create the productModel
+  async create(
+    name: string,
+    price: number,
+    description?: string,
+  ): Promise<ProductDocument> {
+    //instanciate new object by ref to d productModel
+    const newProduct = new this.productModel({ name, price, description });
+    return newProduct.save();
+  }
+
+  async findAll(): Promise<ProductDocument[]> {
+    return this.productModel.find().exec();
+  }
+
+  async find(id: string): Promise<ProductDocument | null> {
+    return this.productModel.findById(id).exec();
+  }
+
+  async update(
+    id: string,
+    newName: string,
+    newPrice: number,
+    newDescription?: string,
+  ): Promise<ProductDocument | null> {
+    let existingProduct = await this.find(id);
+
+    //check if existingProd exists b4 accessing its props
+    if (!existingProduct) {
+      throw new NotFoundException('Product with ID ${d} is missing');
     }
 
-    async findAll(): Promise<ProductDocument[]>{
-        return this.productModel.find().exec()
-    }
+    existingProduct.name = newName ?? existingProduct.name;
+    existingProduct.price = newPrice ?? existingProduct.price;
+    existingProduct.description = newDescription ?? existingProduct.description;
 
-    async find(id : string): Promise<ProductDocument | null>{
-        return this.productModel.findById(id).exec() 
-    }
+    return existingProduct.save();
+  }
 
-    async update(
-        id : string,
-        newName: string, 
-        newPrice: number, 
-        newDescription?: string,
-    ): Promise<ProductDocument | null>{
+  async delete(id: string): Promise<DeleteResult> {
+    return this.productModel.deleteOne({ _id: id }).exec();
+  }
 
-        let existingProduct = await this.find(id) 
-
-        //check if existingProd exists b4 accessing its props
-        if (!existingProduct){
-            throw new NotFoundException('Product with ID ${d} is missing')
-        }
-
-        existingProduct.name = newName ??  existingProduct.name 
-        existingProduct.price = newPrice ??  existingProduct.price 
-        existingProduct.description = newDescription ??  existingProduct.description 
-
-
-     
-        return existingProduct.save()
-    }
-
-    async delete(id: string) : Promise<DeleteResult>{
-        return this.productModel.deleteOne({_id : id}).exec()
-    }
- 
+  async softDelete(id: string) {
+    return this.productModel.softDelete({ id: id });
+  }
 }
-
 
 //@Injectable() means we can inject the fxtionality of this servcie into another file
 //Done by injecting into the constructor of the JS/TS class that its consumed from
